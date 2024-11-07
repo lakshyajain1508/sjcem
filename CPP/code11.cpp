0ca743c7ad357a4d7b7418864c647950836a1a0d
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-07 12:29:44 +0530

    Add files via upload

---
+++ CPP/code11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    
+    int r;
+    double area;
+    cout<<"Enter the radius: - ";
+    cin>>r;
+    area = 3.14*r*r;
+    cout<<"Area of circle is "<<area;
+
+
+    return 0;
+}
