e2477ea7464c1a9829c065eeba1c275dd5ef04b1
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-08 09:53:29 +0530

    Add files via upload

---
+++ CPP/two1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the two numbers -> ";
+    cin>>num1>>num2;
+    cout<< "Addition is - > "<<num1 + num2;
+    return 0;
+    
+}
