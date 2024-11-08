9dd8f06dfb4576fd57444ae9779e564913223fc2
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-08 10:30:02 +0530

    Add files via upload

---
+++ CPP/auto.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    auto data = 0;
+    cout<<"Enter your data : ";
+    cin>>data;
+    cout<<"Data -> "<<data; 
+    return 0;
+
+}
