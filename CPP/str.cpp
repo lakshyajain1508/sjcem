7f3abbe00b15e53e43b644ef8e0375c65a899c5b
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-08 10:02:09 +0530

    Add files via upload

---
+++ CPP/str.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string name = "Lakshya";
+    string lname = "jain";
+    string fullname = name +" "+ lname;
+    cout << "Fullname -> "<<fullname;
+    return 0;
+}
