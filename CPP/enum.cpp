f89db4d276efa0ac88ad50c1cd284315231ba6f9
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-09 14:02:04 +0530

    Add files via upload

---
+++ CPP/enum.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+    enum level{ 
+        Jan,Feb,March
+    };
+  
+int main() { 
+    
+    enum level myvar = March;
+    cout<<myvar;   
+    return 0;                                                                          
+      
+}
