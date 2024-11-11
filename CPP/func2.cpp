93c5473c3906ea08c9284298c9dd9f907dfc96eb
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-11 14:21:45 +0530

    Add files via upload

---
+++ CPP/func2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int func(int a, string b){
+    cout<<"Name -> "<<b<<endl<<"Roll No. -> "<<a;
+    return 0;
+};
+int main(){
+
+    func(10,"Avaneet");
+    return 0;
+}
