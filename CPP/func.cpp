45a4a77ca7efaacacd46fe2e4d4c217176457f02
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-11 13:53:00 +0530

    Update func.cpp

--- CPP/func.cpp
+++ CPP/func.cpp
@@ -2,6 +2,12 @@
 #include<string>
 using namespace std;
 
+void func();
+int main(){
+
+    func();
+    return 0;
+}
 void func(){
     
     int a;
@@ -10,8 +16,3 @@ void func(){
     float area = 3.14*a*a;
     cout<<"Area of circle is "<<area<<endl;
 };
-int main(){
-
-    func();
-    return 0;
-}
