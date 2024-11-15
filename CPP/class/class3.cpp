99bbc9e4c55aae0fa36faf837fd05cc21a712975
Lakshya Jain <lakshyajain1508@gmail.com>
2024-11-15 04:21:46 +0000

    update class3

---
+++ CPP/class/class3.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+ #include <string> 
+ using namespace std; 
+  
+ class Car{
+    public:
+    int speed(int maxspeed){
+        return maxspeed;
+    }
+ };
+ int main(){
+    Car myobj;
+    cout<<myobj.speed(200);
+ }
