6480cb3adba14d2a1f3f44446283e20d7d67e0cf
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-21 09:57:32 +0000

    update java

---
+++ Java Code/narrowing.java
@@ -0,0 +1,16 @@
+package eu1242066;
+
+public class narrowing {
+	
+public static void main(String[] args) {
+		
+		double myDouble = 9.78;
+		int myint = (int) myDouble;
+		
+		
+		System.out.println(myDouble);
+		System.out.println(myint);
+		
+	}
+
+}
