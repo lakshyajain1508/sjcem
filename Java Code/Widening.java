b08531a05865bfa1ce6acaf9f6eade661bef9ce9
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-21 09:54:13 +0000

    update

---
+++ Java Code/Widening.java
@@ -0,0 +1,15 @@
+package eu1242066;
+
+public class Widening {
+
+	public static void main(String[] args) {
+		
+		int myint = 9;
+		double myDouble = myint;
+		
+		System.out.println(myint);
+		System.out.println(myDouble);
+
+	}
+
+}
---
+++ Java Code/plusoperator.java
@@ -0,0 +1,21 @@
+package eu1242066;
+
+public class plusoperator {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 20;
+		int z = x+y;
+		
+		System.out.println(z);
+		
+		String a = "10";
+		String b = "20";
+		String c = a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
