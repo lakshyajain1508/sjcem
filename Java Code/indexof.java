ab81528c805a717b0805d42ac23e1d9baa243cf3
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-21 10:11:01 +0000

    update java code

---
+++ Java Code/indexof.java
@@ -0,0 +1,13 @@
+package eu1242066;
+
+public class indexof {
+	
+	public static void main(String[] args) {
+		
+		String txt = "Please locate where 'loacte' occurs!";
+		System.out.println(txt.indexOf("locate"));
+		
+		
+	}
+	
+}
