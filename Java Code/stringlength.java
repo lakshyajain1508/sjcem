74b31801bde18dbae81afe1a826b2655f25f1efe
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-21 10:01:30 +0000

    update stringlength

---
+++ Java Code/stringlength.java
@@ -0,0 +1,11 @@
+package eu1242066;
+
+public class stringlength {
+
+	public static void main(String[] args) {
+		
+		String txt = "QWERTYUIOPASDFGHJKLZXCVBNM";
+		System.out.println("The Length of the txt string is "+ txt.length());
+		
+	}
+}
