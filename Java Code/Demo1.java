86ee73c91ea28e99be2350f2bfca80968f08d22b
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-21 09:30:01 +0000

    update code

---
+++ Java Code/Demo1.java
@@ -0,0 +1,19 @@
+package eu1242066;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		float b = 120;
+		char c = '&';
+		double d = 12.0;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
---
+++ Java Code/Demo2.java
@@ -0,0 +1,19 @@
+package eu1242066;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		String name = "Hello" ; 
+ 		String surname = "John"; 
+ 		System.out.println(name +" "+ surname ); 
+		
+		
+		int x = 5;
+		int y = 6;
+		
+		System.out.println(x+y);
+
+	}
+
+}
