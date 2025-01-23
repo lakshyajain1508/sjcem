33c68d9cc523b7ee1d9e619f98ac6e2661854273
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-23 10:14:10 +0000

    update switchcase

---
+++ Java Code/switchcase.java
@@ -0,0 +1,24 @@
+package EU1242066;
+
+public class switchcase {
+
+	public static void main(String[] args) {
+		
+		int day = 5;
+		switch(day) {
+		case 2:
+		System.out.println("Tuesday");
+		break;
+		case 4:
+		System.out.println("Thursday");
+		break;
+		default: 
+    	System.out.println("looking for weekends"); 
+    	break;
+			
+		
+		}
+
+	}
+
+}
