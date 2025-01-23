012cffbaef57be3eed73baf833f41cc09ad0d1b8
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-23 09:51:01 +0000

    update code

---
+++ Java Code/ifelse.java
@@ -0,0 +1,22 @@
+package EU1242066;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		
+		int age = 21;
+		if (age<=18) {
+			System.out.println("You are chlid.");
+		}
+		else if(age>=18 && age<=50) {
+			System.out.println("You are adult.");
+		}
+			else {
+				System.out.println("You are old.");
+			}
+			
+		
+
+	}
+
+}
