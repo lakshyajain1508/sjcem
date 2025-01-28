2fdc836129d6c191bbb95d980cb754d38467748a
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-28 09:42:01 +0000

    update userinput string

---
+++ Java Code/UserInput.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter Your Name: - ");
+		String name = scn.nextLine();
+		System.out.println("You entered -> "+name);
+
+	}
+
+}
