162cec11dce7fedc7e44a416dc0bff4f566148a0
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-28 10:11:38 +0000

    Array & foreacharray

---
+++ Java Code/ArrayEx.java
@@ -0,0 +1,14 @@
+
+
+public class ArrayEx {
+	
+	public static void main(String[] args) {
+		
+		String[] data = {"Batch 1","Batch 2","Batch 3"};
+		for(int i = 0; i < data.length;i++) {
+			System.out.println(data[i]);
+		}
+		
+	}
+
+}
---
+++ Java Code/foreacharray.java
@@ -0,0 +1,14 @@
+package EU1242066;
+
+public class foreacharray {
+	
+	public static void main(String[] args) {
+		
+		String[] data = {"Batch 1","Batch 2","Batch 3"};
+		for(String d: data) {
+			System.out.println(d);
+		}
+		
+	}
+
+}
