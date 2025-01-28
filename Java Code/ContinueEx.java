46db54852ac6e10bd17f2d584129250d86597d21
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-28 09:19:50 +0000

    update continue

---
+++ Java Code/ContinueEx.java
@@ -0,0 +1,14 @@
+
+
+public class ContinueEx {
+	
+	public static void main(String[] args) {
+		for(int i = 0;i<11;i++) {
+			if(i==5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
