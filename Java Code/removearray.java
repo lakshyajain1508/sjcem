ea485bf1a0e0206f1e21fa74f1e10133899a64ce
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-28 10:29:13 +0000

    remove array element

---
+++ Java Code/removearray.java
@@ -0,0 +1,17 @@
+package EU1242066;
+import java.util.*; 
+public class removearray { 
+ 
+	public static void main(String[] args) { 
+		String[] data = {"Batch A" , "Batch B" , "Batch C" , "Batch D" , "Batch E" , "Batch F"}; 
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter the position to be Deleted : "); 
+		int a = sc.nextInt(); 
+		for(int i = 0 ; i<7 ; i++) { 
+			if(i==(a-1)) { 
+				continue; 
+			} 
+			System.out.println(data[i]); 
+		} 
+	} 
+}
