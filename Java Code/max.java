f3d45de87346900385afa285e4cb84c853d91a87
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-29 23:04:49 +0530

    update max and min in java

---
+++ Java Code/max.java
@@ -0,0 +1,14 @@
+public class max {
+
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        int max = Math.max(Math.max(num1, num2) , (num3));
+        System.out.println("The maximum number is: " + max);
+
+    }
+    
+}
