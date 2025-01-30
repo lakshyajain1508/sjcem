ca9322b792499bcb7bf2c99abbdf0c3f2aee34ee
Lakshya Jain <lakshyajain1508@gmail.com>
2025-01-30 10:02:04 +0000

    String Example

---
+++ Java Code/StringExample.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class StringExample {
+        public static void main(String[] args) {
+            String name = "Lakshya";
+            String surname = "Jain";
+
+            System.out.println(name.length());
+
+            System.out.println(name.toLowerCase());
+            System.out.println(name.toUpperCase());
+            System.out.println(name.concat(surname));   
+
+            System.out.println(name+" "+surname);
+
+        }
+    
+
+}
