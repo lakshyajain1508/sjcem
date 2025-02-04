22f3508c284041f3678444229cc95c74166d47e5
Lakshya Jain <lakshyajain1508@gmail.com>
2025-02-04 10:08:17 +0000

    Sum of odd position in array

---
+++ Java Code/OddPosition.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class OddPosition {
+
+	public static void main(String[] args) {
+		int array[] = {1,2,3,4,5,6}; 
+		 int sum = 0; 
+		 for(int i = 0 ; i< array.length ; i++ ) 
+		 { 
+		 	if(i%2 != 0) { 
+		 		sum += array[i]; 
+		 } 
+		 	} 
+		 System.out.println(sum); 
+		 	} 
+		 
+	}
+
