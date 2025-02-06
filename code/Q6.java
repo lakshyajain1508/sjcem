a3949d0852c98085ae316eb993e978daf740b648
Lakshya Jain <lakshyajain1508@gmail.com>
2025-02-06 08:22:03 +0000

    code 6

--- code/Q6.java
+++ code/Q6.java
@@ -4,7 +4,25 @@ public class Q6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+/*Voting Eligibility:
+Write a program to check if a person is eligible to vote (age >= 18).*/
+		
+		
+		int age = 19;
+		if(age >= 18)
+		{
+			System.out.println("eligible");
+		}
+		else if(age <= 18)
+		{
+			System.out.println("not eligible");
+		}
+		else
+		{
+			System.out.println("not invalid");
+		}
+		
+		
 	}
 
 }
