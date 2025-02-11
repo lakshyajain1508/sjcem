1f9e9a649a00c0bc31bd2586cd5c46dcbd5060dc
Lakshya Jain <lakshyajain1508@gmail.com>
2025-02-11 09:28:52 +0000

    Split function

---
+++ Java Code/Method/Split.java
@@ -0,0 +1,14 @@
+package EU1242066;
+
+public class Split {
+
+	public static void main(String[] args) {
+		
+		String s = "We are learning java";
+		
+		String st[] = s.split(" ");
+		System.out.println(st[0]);
+
+	}
+
+}
