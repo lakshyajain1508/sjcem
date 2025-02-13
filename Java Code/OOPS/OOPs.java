83d6d1da499cd0bb48347435e2f138fcf096b858
Lakshya Jain <lakshyajain1508@gmail.com>
2025-02-13 09:37:52 +0000

    update OOPS

---
+++ Java Code/OOPS/OOPs.java
@@ -0,0 +1,26 @@
+
+
+class One {
+	
+	String name = "Hello!! Hii Namaste";
+	int num = 10;
+	
+}
+
+public class OOPs {
+	
+	int num = 30;
+
+	public static void main(String[] args) {
+		
+		OOPs obj1 = new OOPs();
+		System.out.println(obj1.num);
+	
+		
+		One obj2 = new One();
+		System.out.println(obj2.num);
+		System.out.println(obj2.name);
+
+	}
+
+}
