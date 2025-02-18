60397521b4db64ef0a8fd368eb29c1e4dd0ea086
Lakshya Jain <lakshyajain1508@gmail.com>
2025-02-18 09:16:37 +0000

    update  OOPs

---
+++ Java Code/OOPS/Constructor01.java
@@ -0,0 +1,20 @@
+
+
+public class Constructor01 {
+	
+	void name(String name) 
+	  { 
+	 	  System.out.println("Your name is "+name); 
+	  } 
+	  public Constructor01() 
+	  { 
+	 	 System.out.println("Hey"); 
+	 	 name("Lakshya."); 
+	  } 
+	 	public static void main(String[] args) { 
+	 		 
+	 		Constructor01 obj1 = new Constructor01(); 
+	  
+	 	} 
+	  
+	 }
