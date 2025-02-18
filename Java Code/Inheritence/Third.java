320b9d92edea0e68bd2992fe2c8438af6a28a829
Lakshya Jain <lakshyajain1508@gmail.com>
2025-02-18 10:10:59 +0000

    third multiple class

---
+++ Java Code/Inheritence/Third.java
@@ -0,0 +1,19 @@
+
+
+public class Third extends Second1{
+	
+	public void ratio() {
+		System.out.println("Hey bro");
+	}
+
+	public static void main(String[] args) {
+		
+		Third th = new Third();
+		th.max();
+		th.min();
+		th.ratio();
+
+		
+	}
+
+}
