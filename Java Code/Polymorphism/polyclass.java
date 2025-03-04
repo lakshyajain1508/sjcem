c669fcab3f61efc2809633a3cb84fe7457aa62cf
Lakshya Jain <lakshyajain1508@gmail.com>
2025-03-04 09:57:03 +0000

    update polymorphism

---
+++ Java Code/Polymorphism/polyclass.java
@@ -0,0 +1,54 @@
+package eu1242066;
+
+
+class Animal{
+	
+	public void animalSound() {
+		System.out.println("Animal Sound");
+	}
+	
+}
+class Dog extends Animal{
+	
+	public void animalSound() {
+		System.out.println("Dog Sound");
+	}
+	
+}
+
+class Cat extends Animal{
+	
+	public void animalSound() {
+		System.out.println("Cat Sound");
+	}
+	
+}
+
+class Rabbit extends Animal{
+	
+	public void animalSound() {
+		System.out.println("Rabbit Sound");
+	}
+	
+}
+
+
+public class polyclass {
+
+	public static void main(String[] args) {
+		
+		Animal objAnimal = new Animal();
+		Animal objDog = new Dog();
+		Animal objCat = new Cat();
+		Animal objRabbit = new Rabbit();
+		
+		objAnimal.animalSound();
+		objDog.animalSound();
+		objCat.animalSound();
+		objRabbit.animalSound();
+		
+		
+
+	}
+
+}
