76fb2f45a95c6386f0ff1a6eecf5bf9edc92341e
Lakshya Jain <lakshyajain1508@gmail.com>
2025-06-25 04:19:01 +0000

    update variable

--- Python/variables.py
+++ Python/variables.py
@@ -1,2 +1,13 @@
 # No variable declaration in program
-# Dynamic Typing - When you create a variable python automatically understand type of datatype 
+# Dynamic Typ - When you create a variable python automatically understand type of datatype.
+# Static Type - We have to specify the datatype and variable type.
+# Accessing sub string from the string.It is called indexing.
+
+var = "Lakshya"
+print(var[0]) # Output- L
+print(var[-4]) # Output- s
+print(var[0:7]) # Output - Lakshya
+
+print(var[12]) # Output - IndexError: string index out of range
+
+# Pratical 1 - Basic Calculator
