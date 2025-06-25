1f852005e1d5fdc086c35aea5e82d4af25f26405
Lakshya Jain <lakshyajain1508@gmail.com>
2025-06-25 05:26:22 +0000

    update arrays

---
+++ Python/array.py
@@ -0,0 +1,6 @@
+'''
+Array represent a group of element having same data type.Hence no need to mention the size of array while creating.
+
+Arrays are two types: 1D or 2D, but python supports 1D to use 2D array use package num.py
+'''
+
