5432647e7c2762c1f9527adc974d82bc28600b6f
Lakshya Jain <lakshyajain1508@gmail.com>
2025-08-21 15:42:25 +0530

    add topic group by and print only name column

--- DBMS5/Practice/staff.sql
+++ DBMS5/Practice/staff.sql
@@ -8,14 +8,12 @@ insert into staff values (001,'Ramesh',15000),
  (003,'Naina',20000),
  (004,'Reena',21000),
  (005,'Mahesh',12000);
-select * from "lakshya"."staff";
 update staff set salary = 18000
 where staff_id = 1;
+select * from staff;
 delete from staff
 where staff_id = 1;
-select from staff;
 alter table staff add age int;
-select * from staff;
 update staff set age = 18
 where staff_id = 2;
 update staff set age = 19
@@ -25,4 +23,12 @@ where staff_id = 4;
 update staff set age = 18
 where staff_id = 5;
 select * from staff;
-drop table staff;
+SELECT age, COUNT(staff_Id) AS number_of_staff
+FROM Staff
+GROUP BY age;
+SELECT age, AVG(salary) AS average_salary
+FROM Staff
+GROUP BY age;
+SELECT name FROM Staff;
+INSERT INTO Staff VALUES (006, 'Priya', 22000, 25);
+select * from staff;
