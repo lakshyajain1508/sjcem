8937266d51c1054aca0ffb405d6cb8c0c50d97cd
Lakshya Jain <lakshyajain1508@gmail.com>
2025-09-20 20:57:21 +0530

    PRACTICE SELECTION SORT

---
+++ Practice/selectionsort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        printf("Data %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
